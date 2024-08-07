6c56f2d1c114c376bf21535d83e25ccb37872cdb
fresh-finest <bb@brecx.com>
2024-08-07 17:07:45 +0600

    created getPrice Schedule

--- src/service/PriceScheduleService.js
+++ src/service/PriceScheduleService.js
@@ -4,3 +4,8 @@ exports.createPriceScheduleService = async(data)=>{
     const schedule = await PriceSchedule.create(data);
     return schedule;
 }
+
+exports.getPriceScheduleService = async()=>{
+    const result = await PriceSchedule.find({});
+    return result;
+}

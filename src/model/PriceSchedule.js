e4282c43ecacc07f80242a63b8f93dd73febe76d
fresh-finest <bb@brecx.com>
2024-08-21 14:26:59 +0600

    schema change-3

--- src/model/PriceSchedule.js
+++ src/model/PriceSchedule.js
@@ -4,11 +4,13 @@ const priceScheduleSchema = mongoose.Schema({
   userName:{type: String},
   asin:{type: String},
   sku: { type: String },
+  title:{type:String},
   price: { type: Number },
   currentPrice: {type:Number},
+  imageURL:{type: String},
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: false },
-})
+}, { timestamps: true })
 
 const PriceSchedule = mongoose.model("Schedule", priceScheduleSchema);
 module.exports = PriceSchedule;

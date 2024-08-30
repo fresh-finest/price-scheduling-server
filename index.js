1eb0a8af1379bf8c24f1dd715b6d5309c0b769b3
fresh-finest <bb@brecx.com>
2024-08-30 13:58:26 +0600

    added amplify

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
+  'https://main.d3iyq7ecbzumwp.amplifyapp.com/',
   'http://localhost:5173',
   'https://changing-price.netlify.app',
   'https://price-changing.netlify.app',

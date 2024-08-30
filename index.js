7a35a1c8d8bc420aadb8e4aa2e38d2d8b7941ab1
fresh-finest <bb@brecx.com>
2024-08-30 15:20:32 +0600

    changed port

--- index.js
+++ index.js
@@ -658,7 +658,7 @@ app.post('/send-email', async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 22;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

8d419a644564e4e7ebfa06fb6f7d4b2496c3eff0
fresh-finest <bb@brecx.com>
2024-09-06 18:45:54 +0600

    port 3000

--- index.js
+++ index.js
@@ -707,7 +707,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const PORT = 80;
+const PORT = 3000;
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
